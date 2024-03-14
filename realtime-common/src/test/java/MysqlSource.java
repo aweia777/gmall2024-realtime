0315364cede0241b7c1ac510499ed655836a4bf7
awei <awei@163.com>
2024-03-15 00:14:42 +0800

    reslove SimpleStringSchema() nullable

---
+++ realtime-common/src/test/java/MysqlSource.java
@@ -0,0 +1,48 @@
+import com.atguigu.gmall.realtime.common.constant.Constant;
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.connectors.mysql.source.MySqlSourceBuilder;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import static org.apache.flink.streaming.api.environment.CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION;
+
+public class MysqlSource {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // 1.3 设置并行度
+        env.setParallelism(4);
+
+
+        MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
+                .hostname(Constant.MYSQL_HOST)
+                .port(Constant.MYSQL_PORT)
+                .username(Constant.MYSQL_USER_NAME)
+                .password(Constant.MYSQL_PASSWORD)
+                .databaseList("gmall2023_config")
+                .tableList("gmall2023_config.table_process_dim")
+                .deserializer(new JsonDebeziumDeserializationSchema())
+                .startupOptions(StartupOptions.initial())
+                .build();
+
+        DataStreamSource<String> mysqlsource = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "mysqlsource");
+
+        mysqlsource.print();
+
+        try {
+            env.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
